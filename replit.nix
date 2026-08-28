8ac788ba99aa9a0fa8e5011c795279036af7342e
Lwandile <lwandilephillgoniwe@gmail.com>
2026-08-28 23:03:02 +0200

    Add replit.nix

---
+++ replit.nix
@@ -0,0 +1,33 @@
+{pkgs}: {
+  deps = [
+    pkgs.xorg.libXScrnSaver
+    pkgs.xorg.libxshmfence
+    pkgs.xorg.libxcb
+    pkgs.xorg.libXi
+    pkgs.xorg.libXtst
+    pkgs.xorg.libXrender
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXext
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libX11
+    pkgs.libxkbcommon
+    pkgs.alsa-lib
+    pkgs.gtk3
+    pkgs.cairo
+    pkgs.pango
+    pkgs.libgbm
+    pkgs.libdrm
+    pkgs.freetype
+    pkgs.fontconfig
+    pkgs.expat
+    pkgs.dbus
+    pkgs.cups
+    pkgs.at-spi2-atk
+    pkgs.atk
+    pkgs.nspr
+    pkgs.nss
+    pkgs.glib
+  ];
+}
